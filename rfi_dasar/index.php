21497505df8e8a3d6f3ac5d1f97645ecbf45b345
Alfian <10alfexp01@gmail.com>
2025-06-19 06:21:15 +0700

    update: perbaikan script RFI

--- rfi_dasar/index.php
+++ rfi_dasar/index.php
@@ -74,7 +74,7 @@ if (isset($_GET['page'])) {
             padding: 15px;
             border-radius: 8px;
             font-family: monospace;
-            white-space: pre-wrap;
+            font-size: 14px;
             border: 1px solid #ccc;
         }
 
